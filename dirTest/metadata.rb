13ec6a57c005d64e2572c3341c2b336346f4737e
ashishraj91 <ashishrajhits@gmail.com>
2016-06-08 17:48:51 +0530

    sample test

---
+++ dirTest/metadata.rb
@@ -0,0 +1,7 @@
+name             'dirTest'
+maintainer       'Relevance Lab Pvt LTD, Inc.'
+maintainer_email 'ashish.raj@relevancelab.com'
+license          'All rights reserved'
+description      'Installs/Configures dirTest'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.3.0'
